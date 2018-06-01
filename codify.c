1ac003908917861f2fa17779dd43cad0b9860c58
Velorek1 <velorek1@gmail.com>
2018-06-02 00:34:34 +0200

    Add files via upload

--- codify.c
+++ codify.c
@@ -1,9 +1,10 @@
 /*
 ===========================================================
 Program that transforms a binary file into a text file
-of integers separated by spaces so it can be sent safely 
-through the internet without getting blocked by antivirus 
-software. The original file can be then reinstated. 
+of integers separated by a SEPARATOR '.' so it can be sent 
+safely through the internet without getting blocked by 
+antivirus software. The original file can be then 
+reinstated. 
 
 @author: Velorek
 @version: 0.1
